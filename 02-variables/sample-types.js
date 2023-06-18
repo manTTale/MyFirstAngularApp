a26f00e9d0de55dec796a384744bb997cba9c69e
manTTale <mantale.roberto1@yahoo.com>
2023-06-18 11:13:43 +0300

    first commit where I played with some variables and some console logs

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,8 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Roberto";
+var lastName = 'Mantale';
+console.log(found);
+console.log("My grade is " + grade);
+console.log('My first name is ' + firstName);
+console.log('My last name is ' + lastName);
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,8 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Roberto";
+let lastName: string = 'Mantale';
+console.log(found);
+console.log("My grade is " + grade);
+console.log('My first name is ' + firstName);
+console.log('My last name is ' + lastName);
